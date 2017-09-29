7546a748d1f69a709bd0bc87b73781733365d089
Vincent Prouillet <vincent@wearewizards.io>
2017-09-29 12:23:10 +0900

    Update dependencies
    Fix #24

--- dbmigrate-lib/src/errors.rs
+++ dbmigrate-lib/src/errors.rs
@@ -8,7 +8,7 @@ use sqlite_client;
 error_chain! {
     foreign_links {
         Io(::std::io::Error) #[doc = "Failed to created/read migration files"];
-        Postgres(postgres_client::error::ConnectError) #[doc = "Couldn't get connection to pg database"] #[cfg(feature = "postgres_support")];
+        Postgres(postgres_client::error::Error) #[doc = "Couldn't get connection to pg database"] #[cfg(feature = "postgres_support")];
         MySQL(mysql_client::Error) #[doc = "Any MySQL error"] #[cfg(feature = "mysql_support")];
         Sqlite(sqlite_client::Error) #[doc = "Any Sqlite error"] #[cfg(feature = "sqlite_support")];
     }
